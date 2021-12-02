baa43ddccf382b27ec057645576e58127914aad7
Jrryy <goteroma@redhat.com>
2021-12-02 10:33:09 +0100

    Day 2 done

---
+++ day_2/code.py
@@ -0,0 +1,16 @@
+with open('input.txt', 'r') as f:
+    x, y, y_2 = 0, 0, 0
+    for instruction in f:
+        direction, units_str = instruction.split(' ')
+        units = int(units_str)
+        if direction == 'forward':
+            x += units
+            y_2 += units * y
+        elif direction == 'down':
+            y += units
+        elif direction == 'up':
+            y -= units
+    product_1 = x * y
+    product_2 = x * y_2
+    print(product_1)
+    print(product_2)
