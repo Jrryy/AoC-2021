4510390f3993e874f60c4db176ead20fd97edfba
Jrryy <jerry.om.95@gmail.com>
2021-12-14 23:13:28 +0100

    Day 14 done

--- day_14/code.py
+++ day_14/code.py
@@ -1,27 +1,53 @@
+from collections import defaultdict
+
 transformations = {}
+pairs = {}
 
 with open('input.txt', 'r') as f:
     list_f = list(f)
     template = list_f[0].strip()
+    last_char = template[-1]
     for pair, element in map(lambda x: x.strip().split(' -> '), list_f[2:]):
         transformations[pair] = element
+        pairs[pair] = 0
+
+for i in range(len(template) - 1):
+    pairs[template[i:i + 2]] += 1
 
 for _ in range(10):
-    new_template = ''
-    for i in range(len(template) - 1):
-        new_template += template[i] + transformations[template[i:i + 2]]
-    template = new_template + template[-1]
+    new_pairs = defaultdict(int)
+    for pair, frequency in pairs.items():
+        inserted = transformations[pair]
+        new_pairs[pair[0] + inserted] += frequency
+        new_pairs[inserted + pair[1]] += frequency
+    pairs = new_pairs
 
-chars_frequence = sorted([template.count(x) for x in set(template)])
+singles = defaultdict(int)
 
-print(f'Part 1: {chars_frequence[-1] - chars_frequence[0]}')
+for pair, frequency in pairs.items():
+    singles[pair[0]] += frequency
+
+sorted_singles = sorted(singles.items(), key=lambda x: x[1])
+least_frequent = sorted_singles[0][1] + 1 if sorted_singles[0][0] == last_char else sorted_singles[0][1]
+most_frequent = sorted_singles[-1][1] + 1 if sorted_singles[-1][0] == last_char else sorted_singles[-1][1]
+
+print(f'Part 1: {most_frequent - least_frequent}')
 
 for _ in range(30):
-    new_template = ''
-    for i in range(len(template) - 1):
-        new_template += template[i] + transformations[template[i:i + 2]]
-    template = new_template + template[-1]
+    new_pairs = defaultdict(int)
+    for pair, frequency in pairs.items():
+        inserted = transformations[pair]
+        new_pairs[pair[0] + inserted] += frequency
+        new_pairs[inserted + pair[1]] += frequency
+    pairs = new_pairs
 
-chars_frequence = sorted([template.count(x) for x in set(template)])
+singles = defaultdict(int)
 
-print(f'Part 2: {chars_frequence[-1] - chars_frequence[0]}')
+for pair, frequency in pairs.items():
+    singles[pair[0]] += frequency
+
+sorted_singles = sorted(singles.items(), key=lambda x: x[1])
+least_frequent = sorted_singles[0][1] + 1 if sorted_singles[0][0] == last_char else sorted_singles[0][1]
+most_frequent = sorted_singles[-1][1] + 1 if sorted_singles[-1][0] == last_char else sorted_singles[-1][1]
+
+print(f'Part 2: {most_frequent - least_frequent}')
